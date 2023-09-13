d4d126926b3c651319cfded9809d833c718d0f37
unknown <dinielmi912@gmail.com>
2023-09-13 14:01:57 +0700

    modifikasi anggota 1

--- FirstGithubCollab.java
+++ FirstGithubCollab.java
@@ -4,6 +4,6 @@ public class FirstGithubCollab {
         System.out.println("Ini ceritanya fitur pertama sudah berjalan baik");
 
         System.out.println("Ketua mulai mengembangkan fitur kedua");
-        System.out.println(x:"Tambahan dari anggota 1");
+        System.out.println("Tambahan dari anggota 1");
     }
 }
