a0afefafaa91b96249f6fef581afe3eb06fd7dad
KHOIRKAROLNURZURAIDAH <khoirkarolnurzuraidah23@gmail.com>
2023-09-13 14:55:08 +0700

    eror

--- FirstGithubCollab.java
+++ FirstGithubCollab.java
@@ -1,12 +1,10 @@
 public class FirstGithubCollab {
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
         System.out.println("Ini ceritanya fitur pertama sudah berjalan baik");
 
         System.out.println("Ketua mulai mengembangkan fitur kedua");
         System.out.println("Tambahan dari anggota 1");
         System.out.println("Tambahan dari anggota 2");
-        System.out.println("Tambahan dari anggota 3");
-    
-    }   
-}       
+    }
+}
