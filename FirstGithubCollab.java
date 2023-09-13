dd0ebca90587a874bbd6595f581ec878474cb018
flaxelyn <fallujahrc@gmail.com>
2023-09-13 13:28:09 +0700

    fitur ke 2

--- FirstGithubCollab.java
+++ FirstGithubCollab.java
@@ -2,5 +2,8 @@ public class FirstGithubCollab {
 
     public static void main(String [] args){
         System.out.println("Ini ceritanya fitur pertama sudah berjalan baik");
+
+        System.out.println("Ketua mulai mengembangkan fitur kedua");
+        
     }
 }
