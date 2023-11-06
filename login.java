a4a226cff04af6737a7e4c2f0c4f71c29ef518fb
flaxelyn <fallujahrc@gmail.com>
2023-11-06 13:25:33 +0700

    feat: pengembalian buku dan input tanggal

--- login.java
+++ login.java
@@ -1,11 +1,17 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class login {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        SimpleDateFormat tanggalFormat = new SimpleDateFormat("dd-MM-yyyy");
 
         int entry, pilihMahasiswa, layananPustakawan;
-        String loginMasuk, judulBuku;
+        String loginMasuk, judulBuku, tanggalPeminjaman, tanggalPengembalian;
+        Date tglPengembalian, tglPeminjaman;
+        long selisihHari;
 
         do {
             System.out.println("||      Selamat Datang di Perpustakaan JTI               ||");
@@ -48,6 +54,36 @@ public class login {
                                 int meminjam = input.nextInt();
                                 System.out.printf("||    Mahasiswa %s dengan nim %s meminjam buku %s sejumlah %s \n", nama, nim, judulBuku, meminjam);
                                 System.out.println("");
+                                break;
+                            
+                            case 2:
+                            judulBuku = input.nextLine();
+                                System.out.println("===========================================================");
+                                System.out.print("||    Masukan judul buku yang ingin di pinjam : ");
+                                judulBuku = input.nextLine();
+                                try {
+                                    System.out.print("||    Masukan tanggal peminjaman (dd-MM-yyyy) : ");
+                                    tanggalPeminjaman = input.next();
+                                    tglPeminjaman = tanggalFormat.parse(tanggalPeminjaman);
+                                    System.out.print("||    Masukan tanggal pengembalian (dd-MM-yyyy) : ");
+                                    tanggalPengembalian = input.next();
+                                    tglPengembalian = tanggalFormat.parse(tanggalPengembalian);
+
+                                    selisihHari = (tglPengembalian.getTime() - tglPeminjaman.getTime()) / (24 * 60 * 60 * 1000);
+
+                                    if (selisihHari > 7 ){
+                                        System.out.printf("||    Mahasiswa %s terlambat mengembalikan buku dan dikenakan denda", nama);
+                                        System.out.println("");
+                                    } else {
+                                        System.out.println("||    Terimakasih telah mengembalikan tepat waktu!");
+                                        System.out.println("");
+                                    }
+
+                                } catch (ParseException e){
+                                    System.out.println("Format tanggal tidak sesuai. Gunakan format dd-MM-yyyy");
+                                }
+                            
+                            case 3:
                                 break;
                                 
                                 default:
